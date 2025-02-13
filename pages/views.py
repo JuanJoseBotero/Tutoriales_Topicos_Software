b904c66f55c203799d62d1518dbc17efbf37651c
juan jose botero garcía <jjboterog@eafit.edu.co>
2025-02-13 15:27:27 -0500

    Activities 6-10

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
+from django.core.exceptions import ValidationError
+import re
 
 
 # Create your views here.
@@ -42,10 +45,10 @@ class ContactPageView(TemplateView):
  
 class Product: 
     products = [ 
-        {"id":"1", "name":"TV", "description":"Best TV", "price":"600"}, 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":"500"}, 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":"50"}, 
-        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":"30"} 
+        {"id":"1", "name":"TV", "description":"Best TV", "price": 600}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 500}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 50}, 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 30} 
     ] 
  
 class ProductIndexView(View): 
@@ -73,4 +76,45 @@ class ProductShowView(View):
         viewData["subtitle"] =  product["name"] + " - Product information" 
         viewData["product"] = product 
  
+        return render(request, self.template_name, viewData)
+ 
+class ProductForm(forms.Form): 
+    name = forms.CharField(required=True) 
+    price = forms.FloatField(required=True) 
+
+    def clean_price(self):  # Django ejecuta este método automáticamente
+        price = self.cleaned_data.get("price")  # ✅ Obtener "price" correctamente
+
+        if price < 0:  # ✅ Verificar si el precio es negativo
+            raise ValidationError("El precio no debe ser negativo.")
+
+        return price
+ 
+ 
+class ProductCreateView(View): 
+    template_name = 'products/create.html' 
+ 
+    def get(self, request): 
+        form = ProductForm() 
+        viewData = {} 
+        viewData["title"] = "Create product" 
+        viewData["form"] = form 
         return render(request, self.template_name, viewData) 
+ 
+    def post(self, request): 
+        form = ProductForm(request.POST) 
+        if form.is_valid():
+            return redirect('success')  
+        else: 
+            viewData = {} 
+            viewData["title"] = "Create product" 
+            viewData["form"] = form 
+            return render(request, self.template_name, viewData)
+        
+class SuccessView(View):
+    template_name = 'pages/success.html'
+    
+    def get(self, request):
+        return render(request, self.template_name)
+        
+        
